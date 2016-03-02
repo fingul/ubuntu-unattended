e3794e05cf6bd3eb93d99647c6f9f1279b6234af
fingul <fingul@gmail.com>
2016-03-03 01:15:35 +0900

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
-sudo apt-get update -y
+#sudo apt-get update -y
 sudo apt-get install -y git
-if [[ ! -f ubuntu-unattended ]]; then
+if [[ ! -f ./ubuntu-unattended ]]; then
   git clone https://github.com/fingul/ubuntu-unattended.git
 fi
 cd ubuntu-unattended
