a25fa944066a5c9ece055388ef858d1d37a6b9f3
m <fingul@gmail.com>
2016-03-03 02:22:02 +0900

    .

--- create-unattended-iso.sh
+++ create-unattended-iso.sh
@@ -7,34 +7,17 @@ timezone="${timezone:-Asia/Seoul}"
 username="${username:-username}"
 password="${password:-password}"
 mirror="${mirror:-ftp.daumkakao.com}"
-bootable="${bootable:-yes}"
 download="${download:-http://ftp.daumkakao.com/ubuntu-releases/}"
 download_file="ubuntu-14.04.4-server-amd64.iso"             # filename of the iso to be downloaded
 download_location="http://ftp.daumkakao.com/ubuntu-releases/14.04/"     # location of the file to be downloaded
 new_iso_name="${download_file}.unattended.iso"   # filename of the new iso file to be created
 autostart=true
-github_repo="https://github.com/fingul/ubuntu-unattended/raw/master"
-seed_file="guided-lvm.seed"
-# define spinner function for slow tasks
-# courtesy of http://fitnr.com/showing-a-bash-spinner.html
-spinner()
-{
-    local pid=$1
-    local delay=0.75
-    local spinstr='|/-\'
-    tput civis;
+seed_file="/vagrant/guided-lvm.seed"
+seed_file_name="myfile.seed"
+dir_iso="/vagrant"
+outfile="/vagrant/$new_iso_name"
 
-    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-        local temp=${spinstr#?}
-        printf " [%c]  " "$spinstr"
-        local spinstr=$temp${spinstr%"$temp"}
-        sleep $delay
-        printf "\b\b\b\b\b\b"
-    done
-
-    printf "    \b\b\b\b"
-    tput cnorm;
-}
+rm -rf $outfile
 
 # define download function
 # courtesy of http://fitnr.com/showing-file-download-progress-using-wget.html
@@ -73,26 +56,22 @@ if [ ${UID} -ne 0 ]; then
     exit 1
 fi
 
+
+echo "check dir $dir_iso/$download_file"
 # download the ubunto iso
-cd $tmp
-if [[ ! -f $tmp/$download_file ]]; then
-    echo -n " downloading $download_file: "
-    download "$download_location$download_file"
+if [[ ! -f $dir_iso/$download_file ]]; then
+    todown="$download_location$download_file"
+    echo download "$todown to $dir_iso/$download_file"
+    wget -nv -O $dir_iso/$download_file $todown
 fi
 
-# download lvm seed file
-if [[ ! -f $tmp/$seed_file ]]; then
-    echo -n " downloading $seed_file: "
-    download "$github_repo/$seed_file"
-fi
+cd $tmp
 
 # install required packages
 echo " installing required packages"
 if [ $(program_is_installed "mkpasswd") -eq 0 ] || [ $(program_is_installed "mkisofs") -eq 0 ]; then
-    (apt-get -y update > /dev/null 2>&1) &
-    spinner $!
-    (apt-get -y install whois genisoimage > /dev/null 2>&1) &
-    spinner $!
+    apt-get -y update
+    apt-get -y install whois genisoimage
 
     # thanks to rroethof
     if [ -f /usr/bin/mkisofs ]; then
@@ -106,11 +85,13 @@ mkdir -p $tmp
 mkdir -p $tmp/iso_org
 mkdir -p $tmp/iso_new
 
+
 # mount the image
 if grep -qs $tmp/iso_org /proc/mounts ; then
     echo " image is already mounted, continue"
 else
-    mount -o loop $tmp/$download_file $tmp/iso_org
+    cp $dir_iso/$download_file /tmp/0.iso
+    mount -o loop /tmp/0.iso $tmp/iso_org
 fi
 
 # copy the iso contents to the working directory
@@ -126,16 +107,16 @@ if $autostart ; then
 fi
 
 # set late command
-late_command="chroot /target wget -O /home/$username/init.sh $github_repo/init.sh ;\
-    chroot /target chmod +x /home/$username/init.sh ;"
+#late_command="chroot /target wget -O /home/$username/init.sh $github_repo/init.sh ;\
+#    chroot /target chmod +x /home/$username/init.sh ;"
 
 # copy the netson seed file to the iso
-cp -rT $tmp/$seed_file $tmp/iso_new/preseed/$seed_file
+cp -rT $seed_file $tmp/iso_new/preseed/$seed_file_name
 
 # include firstrun script
-echo "
-# setup firstrun script
-d-i preseed/late_command                                    string      $late_command" >> $tmp/iso_new/preseed/$seed_file
+#echo "
+## setup firstrun script
+#d-i preseed/late_command                                    string      $late_command" >> $tmp/iso_new/preseed/$seed_file
 
 # generate the password hash
 pwhash=$(echo $password | mkpasswd -s -m sha-512)
@@ -143,49 +124,36 @@ pwhash=$(echo $password | mkpasswd -s -m sha-512)
 # update the seed file to reflect the users' choices
 # the normal separator for sed is /, but both the password and the timezone may contain it
 # so instead, I am using @
-sed -i "s@{{username}}@$username@g" $tmp/iso_new/preseed/$seed_file
-sed -i "s@{{pwhash}}@$pwhash@g" $tmp/iso_new/preseed/$seed_file
-sed -i "s@{{hostname}}@$hostname@g" $tmp/iso_new/preseed/$seed_file
-sed -i "s@{{timezone}}@$timezone@g" $tmp/iso_new/preseed/$seed_file
-sed -i "s@{{mirror}}@$mirror@g" $tmp/iso_new/preseed/$seed_file
+sed -i "s@{{username}}@$username@g" $tmp/iso_new/preseed/$seed_file_name
+sed -i "s@{{pwhash}}@$pwhash@g" $tmp/iso_new/preseed/$seed_file_name
+sed -i "s@{{hostname}}@$hostname@g" $tmp/iso_new/preseed/$seed_file_name
+sed -i "s@{{timezone}}@$timezone@g" $tmp/iso_new/preseed/$seed_file_name
+sed -i "s@{{mirror}}@$mirror@g" $tmp/iso_new/preseed/$seed_file_name
 
 
 # calculate checksum for seed file
-seed_checksum=$(md5sum $tmp/iso_new/preseed/$seed_file)
+seed_checksum=$(md5sum $tmp/iso_new/preseed/$seed_file_name)
 
 # add the autoinstall option to the menu
 sed -i "/label install/ilabel autoinstall\n\
   menu label ^Unattended Ubuntu Server Install\n\
   kernel /install/vmlinuz\n\
-  append file=/cdrom/preseed/ubuntu-server-minimalvm.seed initrd=/install/initrd.gz auto=true priority=high preseed/file=/cdrom/preseed/${seed_file} preseed/file/checksum=$seed_checksum --" $tmp/iso_new/isolinux/txt.cfg
+  append file=/cdrom/preseed/ubuntu-server-minimalvm.seed initrd=/install/initrd.gz auto=true priority=high preseed/file=/cdrom/preseed/${seed_file_name} preseed/file/checksum=$seed_checksum --" $tmp/iso_new/isolinux/txt.cfg
 
 echo " creating the remastered iso"
 cd $tmp/iso_new
-(mkisofs -D -r -V "Ubuntu server" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o $tmp/$new_iso_name . > /dev/null 2>&1) &
-spinner $!
+mkisofs -D -r -V "Ubuntu server" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o $outfile .
 
 # print info to user
 echo " -----"
 echo " finished remastering your ubuntu iso file"
-echo " the new file is located at: $tmp/$new_iso_name"
+echo " the new file is located at: $outfile"
 echo " your username is: $username"
 echo " your password is: $password"
 echo " your hostname is: $hostname"
 echo " your timezone is: $timezone"
 echo
 
-# unset vars
-unset username
-unset password
-unset password2
-unset hostname
-unset timezone
-unset pwhash
-unset download_file
-unset download_location
-unset new_iso_name
-unset tmp
-unset seed_file
 
 # cleanup
 umount $tmp/iso_org
